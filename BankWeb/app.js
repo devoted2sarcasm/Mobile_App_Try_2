63ea09354e5c6bcf6b1bded4b56f6b963ebdf5a1
devoted2sarcasm <97539267+devoted2sarcasm@users.noreply.github.com>
2023-07-09 22:21:35 -0400

    PWA stuff

--- BankWeb/app.js
+++ BankWeb/app.js
@@ -1,43 +1,9 @@
-// Import required modules
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-
-// Create an instance of the Express.js server
-const app = express();
-
-// Create a SQLite database connection
-const db = new sqlite3.Database('database.db');
-
-// Define routes and handlers
-app.get('/users', (req, res) => {
-  // Retrieve users from the database
-  db.all('SELECT * FROM users', (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(rows);
-    }
-  });
-});
-
-app.post('/users', (req, res) => {
-  // Extract user data from the request body
-  const { name, email } = req.body;
-
-  // Insert a new user into the database
-  db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.sendStatus(201); // Created
-    }
-  });
-});
-
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
--- BankWeb/service-worker.js
+++ BankWeb/service-worker.js
@@ -1,35 +1,66 @@
+const cacheName = 'ftw-bank';
+
 self.addEventListener("install", function(event) {
     event.waitUntil(
-      caches.open("ftw-bank").then(function(cache) {
-        return cache.addAll([
-          "/",
-          "index.html",
-          "style.css",
-          "images/bank-icon.png",
-          "accountinfo.html",
-          "createaccount.html",
-          "accounthistory.html",
-          "script-history.js",
-          "script-create-mk4.js",
-          "script-account-mk3.js",
-          "script-index-mk3.js",
-          "manifest.json",
-          "service-worker.js",
-          "images/acct_bg.png",
-          "images/bank2.ico",
-          "images/bank2.png",
-          "images/blank.png",
-          // Add other files you want to cache
-        ]);
-      })
+      caches.open(cacheName).then(function(cache) {
+        cache.add("./");
+        console.log("cache added");
+        cache.add("./index.html");
+        console.log('index.html added');
+        cache.add("./style.css");
+        console.log('style.css added');
+        cache.add("./accountinfo.html");
+        console.log('accountinfo.html added');
+        cache.add("./createaccount.html");
+        console.log('createaccount.html added');
+        cache.add("./accounthistory.html");
+        console.log('accounthistory.html added');
+        cache.add("./script-history.js");
+        console.log('script-history.js added');
+        cache.add("./script-create-mk4.js");
+        console.log('script-create-mk4.js added');
+        cache.add("./script-account-mk3.js");
+        console.log('script-account-mk3.js added');
+        cache.add("./script-index-mk3.js");
+        console.log('script-index-mk3.js added');
+        cache.add("./manifest.json");
+        console.log('manifest.json added');
+        cache.add("./service-worker.js");
+        console.log('service-worker.js added');
+        cache.add("images/acct_bg.png");
+        console.log('./images/acct_bg.png added');
+        cache.add("images/bank2.ico");
+        console.log('./images/bank2.ico added');
+        cache.add("images/bank2.png");
+        console.log('./images/bank2.png added');
+        cache.add("images/blank.png");
+        console.log('./images/blank.png added');
+
+        return cache;
+        })
     );
-  });
+      });
   
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.filter(name => name !== cacheName)
+            .map(name => caches.delete(name))
+        );
+      })
+      .then(() => {
+        self.clients.claim();
+      })
+  );
+});
+  
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
         return response || fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
